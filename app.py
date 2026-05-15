222d3584ac844e00b9e92cdfa08b152983ef9ab7
anita <anita.bacova@gmail.com>
2026-05-15 16:43:14 +0200

    e_shop_2

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import jwt
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://neondb_owner:npg_I5zs0OAwrtVm@ep-old-sun-ala7ftma-pooler.c-3.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://neondb_owner:...
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 SECRET_KEY = 'tajny_kluc'
